c60ac21bd574cb952c103dc22272c73ea1233073
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2022-10-14 11:18:14 +0300

    Checkpoint

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 [ ! -d "build" ] && mkdir build
 [ ! -d "target" ] && mkdir target
 
-g++ -o build/raytracing src/main.cpp && build/raytracing > target/image.ppm
+g++ -o build/raytracing src/main.cpp
+
+build/raytracing > target/image.ppm
